d3c75249f29732dde6d7d170daf4874b154bbc6b
christianbillp <christianbillp@gmail.com>
2018-11-21 01:02:59 +0100

    Example visualizations in main.py

---
+++ fake_os.py
@@ -0,0 +1,12 @@
+#%%
+def system(command):
+    print(command)
+    
+def popen(command):
+    return popened()
+
+class popened():
+    
+    def read(self):
+        return "Some program output"
+
